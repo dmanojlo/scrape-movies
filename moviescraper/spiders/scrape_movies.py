e3efc8b7762d53ce5d0d915b84e1cc3d2cf02171
dmanojlo <dragojlo@net.hr>
2021-09-29 10:13:24 +0200

    ini

--- moviescraper/spiders/scrape_movies.py
+++ moviescraper/spiders/scrape_movies.py
@@ -14,9 +14,9 @@ class TopRatedMovies(scrapy.Spider):
 
     start_urls = ['https://www.imdb.com/chart/top/']
 
-    custom_settings = { 'FEEDS': 'result.json': {'format': 'json', 'overwrite': True}}}
+    custom_settings = { 'FEEDS': {'items/results/result.json': {'format': 'json', 'overwrite': True}}}
 
     def parse(self, response):
         a = random.randint(0,249)
         movie_name = response.css('.titleColumn a::text')[a].get()
-        yield {'movie_name': movie_name, 'jobid': os.getcwd()}
+        yield {'movie_name': movie_name, 'jobid': self.jobId}
